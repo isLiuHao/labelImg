39351d4bf61f04866ba576777766118a451278c8
isLiuHao <603400054@qq.com>
2021-06-23 20:15:51 +0800

    矫正后重命名

--- tools/x2coco.py
+++ tools/x2coco.py
@@ -244,7 +244,7 @@ def voc_get_coco_annotation(obj, label2id):
         'area': o_width * o_height,
         'iscrowd': 0,
         'bbox': [xmin, ymin, o_width, o_height],
-        'category_id': category_id,
+        'category_id': category_id-1,
         'ignore': 0,
     }
     return anno
@@ -277,7 +277,7 @@ def voc_xmls_to_cocojson(annotation_paths, label2id, output_dir, output_file):
             bnd_id = bnd_id + 1
 
     for label, label_id in label2id.items():
-        category_info = {'supercategory': 'none', 'id': label_id, 'name': label}
+        category_info = {'supercategory': 'none', 'id': label_id-1, 'name': label}
         output_json_dict['categories'].append(category_info)
     output_file = os.path.join(output_dir, output_file)
     with open(output_file, 'w') as f:
