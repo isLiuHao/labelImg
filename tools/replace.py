d86ecef3251cb1a91cb74fecf8fd2fe79445864b
isLiuHao <603400054@qq.com>
2021-06-30 20:06:09 +0800

    矫正后重命名

--- tools/replace.py
+++ tools/replace.py
@@ -1,10 +1,12 @@
-
-in_path = 'I:/Images_OCR/after_image/Voc_med/test.txt'
-out_path = 'I:/Images_OCR/after_image/Voc_med/testvoc2coco.txt'
+import argparse
 
 if __name__ == '__main__':
-    f_open = open(in_path, 'r')  # 读取训练集
-    f_write = open(out_path, 'w')  # 将替换后的写到新文本内
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--in_path',default='I:/Images_OCR/after_image/Voc_med/test.txt')
+    parser.add_argument('--out_path',default='I:/Images_OCR/after_image/Voc_med/testvoc2coco.txt')
+    args = parser.parse_args()
+    f_open = open(args.in_path, 'r')  # 读取训练集
+    f_write = open(args.out_path, 'w')  # 将替换后的写到新文本内
     lines = f_open.readlines()
     lists = ''
     for line in lines:
@@ -14,5 +16,5 @@ if __name__ == '__main__':
     f_write.write(lists)
     f_open.close()
     f_write.close()
-    print(in_path.split('/')[-1] + '=========》' + out_path.split('/')[-1] + '转化完毕')
+    print(args.in_path.split('/')[-1] + '=========》' + args.out_path.split('/')[-1] + '转化完毕')
 
